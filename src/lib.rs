001d956985b52e9b823546d62b0adb62b4e35701
Costea Chitorog <pankerbest@gmail.com>
2022-05-20 00:40:16 +0300

    new api

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,111 @@
 pub mod mem;
-use std::{cell::{RefCell}, sync::Arc};
-
 use neon::{prelude::*, types::buffer::TypedArray};
-    
-fn open_process(mut cx: FunctionContext) -> JsResult<JsBox<Arc<mem::Process>>> {
+use windows::Win32::Foundation::*;
+
+fn open_process(mut cx: FunctionContext) -> JsResult<JsObject> {
     let processName = cx.argument::<JsString>(0)?.value(&mut cx);
-    let process = mem::Process::new(processName.as_str()).unwrap();
-    Ok(cx.boxed(Arc::new(process)))
+    let process = mem::open_process(processName.as_str()).unwrap();
+    let mut obj = cx.empty_object();
+
+    let id = cx.number(process.id);
+    let name = cx.string(&process.name);
+    let handle = cx.number(process.handle as f64);
+    let path = cx.string(&process.path);
+
+    obj.set(&mut cx, "id", id)?;
+    obj.set(&mut cx, "name", name)?;
+    obj.set(&mut cx, "handle", handle)?;
+    obj.set(&mut cx, "path", path)?;
+
+    Ok(obj)
 }
 
 fn close_handle(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let process = cx.argument::<JsBox<Arc<mem::Process>>>(0)?;
-    process.close_handle();
+    let handle = cx.argument::<JsNumber>(0)?.value(&mut cx);
+    mem::close_handle(HANDLE(handle as isize));
     Ok(cx.undefined())
 }
 
 fn sig_scan(mut cx: FunctionContext) -> JsResult<JsPromise> {
-    let process = cx.argument::<JsBox<Arc<mem::Process>>>(0)?.clone();
+    let handle = cx.argument::<JsNumber>(0)?.value(&mut cx);
     let pattern = cx.argument::<JsString>(1)?.value(&mut cx);
     let address = cx.argument::<JsNumber>(2)?.value(&mut cx) as u64;
 
-    let process = Arc::clone(&process);
-
-    let promise = cx.task(move|| {
-        process.sig_scan(&pattern, address)
-    }).promise::<JsValue, _>(move|mut cx, result| {
-        match result {
-            Some(x) => {
-                Ok(cx.number(x as f64).upcast())
-            }
-            None => {
-                Ok(cx.undefined().upcast())
-            }
-        }
-    });
+    let promise = cx
+        .task(move || mem::sig_scan(HANDLE(handle as isize), &pattern, address))
+        .promise::<JsValue, _>(move |mut cx, result| match result {
+            Some(x) => Ok(cx.number(x as f64).upcast()),
+            None => Ok(cx.undefined().upcast()),
+        });
     Ok(promise)
 }
 
 fn read_memory(mut cx: FunctionContext) -> JsResult<JsBuffer> {
-    let process = cx.argument::<JsBox<RefCell<mem::Process>>>(0)?;
+    let handle = cx.argument::<JsNumber>(0)?.value(&mut cx);
     let address = cx.argument::<JsNumber>(1)?.value(&mut cx) as u64;
     let size = cx.argument::<JsNumber>(2)?.value(&mut cx) as usize;
-    let process = process.borrow();
-    let buffer = process.read_memory(address, size);
+    let buffer: Vec<u8> = mem::read_memory_buffer(HANDLE(handle as isize), address, size);
     let a = JsBuffer::external(&mut cx, buffer);
     Ok(a)
 }
 
 fn write_memory(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let process = cx.argument::<JsBox<RefCell<mem::Process>>>(0)?;
+    let handle = cx.argument::<JsNumber>(0)?.value(&mut cx);
     let address = cx.argument::<JsNumber>(1)?.value(&mut cx) as u64;
     let data = cx.argument::<JsBuffer>(2)?.as_slice(&cx).to_vec();
-    let process = process.borrow();
-    process.write_memory(address, &data).unwrap();
+    mem::write_memory_buffer(HANDLE(handle as isize), address, &data);
     Ok(cx.undefined())
 }
 
+fn alloc_memory(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    let handle = cx.argument::<JsNumber>(0)?.value(&mut cx);
+    let size = cx.argument::<JsNumber>(1)?.value(&mut cx) as usize;
+    let address = mem::alloc_memory(HANDLE(handle as isize), size);
+    Ok(cx.number(address as f64))
+}
 
+fn get_modules(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let process_id = cx.argument::<JsNumber>(0)?.value(&mut cx);
+    let modules = mem::get_modules(process_id as u32).unwrap();
+    let obj = cx.empty_object();
+    let mut i = 0;
+    for module in modules {
+        let module_obj = cx.empty_object();
+        let id = cx.number(i as f64);
+        let name = cx.string(&module.name);
+        let base = cx.number(module.base_address as f64);
+        let size = cx.number(module.size as f64);
+        let path = cx.string(&module.path);
+
+        module_obj.set(&mut cx, "id", id)?;
+        module_obj.set(&mut cx, "name", name)?;
+        module_obj.set(&mut cx, "base", base)?;
+        module_obj.set(&mut cx, "size", size)?;
+        module_obj.set(&mut cx, "path", path)?;
+
+        obj.set(&mut cx, i, module_obj)?;
+        i += 1;
+    }
+    Ok(obj)
+}
+
+fn get_processes(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let processes = mem::get_processes().unwrap();
+    let obj = cx.empty_object();
+    let mut i = 0;
+    for process in processes {
+        let process_obj = cx.empty_object();
+        let id = cx.number(process.id as f64);
+        let name = cx.string(&process.name);
+
+        process_obj.set(&mut cx, "id", id)?;
+        process_obj.set(&mut cx, "name", name)?;
+
+        obj.set(&mut cx, i, process_obj)?;
+        i += 1;
+    }
+    Ok(obj)
+}
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
@@ -65,5 +114,8 @@ fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("sig_scan", sig_scan)?;
     cx.export_function("read_memory", read_memory)?;
     cx.export_function("write_memory", write_memory)?;
+    cx.export_function("alloc_memory", alloc_memory)?;
+    cx.export_function("get_modules", get_modules)?;
+    cx.export_function("get_processes", get_processes)?;
     Ok(())
 }
--- src/mem.rs
+++ src/mem.rs
@@ -1,173 +1,254 @@
-use std::mem::size_of;
-use std::{ ptr};
-use neon::prelude::Context;
-use skidscan::{Signature};
-use neon::types::Finalize;
+use std::ptr::null;
+use std::{mem, ptr};
 
-use winapi::um::handleapi::CloseHandle;
-use winapi::um::memoryapi::{ReadProcessMemory, VirtualQueryEx, WriteProcessMemory};
-use winapi::um::processthreadsapi::OpenProcess;
-use winapi::um::winnt::{
-    HANDLE, MEM_COMMIT, MEM_IMAGE, MEMORY_BASIC_INFORMATION, PROCESS_ALL_ACCESS,
-};
-use winapi::um::tlhelp32::{
-    CreateToolhelp32Snapshot, Process32NextW, PROCESSENTRY32W, TH32CS_SNAPPROCESS,
+use skidscan::Signature;
+
+use windows::Win32::Foundation::*;
+use windows::Win32::System::Diagnostics::Debug::ReadProcessMemory;
+use windows::Win32::System::Diagnostics::Debug::WriteProcessMemory;
+
+use windows::Win32::System::Diagnostics::ToolHelp::CreateToolhelp32Snapshot;
+use windows::Win32::System::Diagnostics::ToolHelp::Module32NextW;
+use windows::Win32::System::Diagnostics::ToolHelp::Process32NextW;
+use windows::Win32::System::Diagnostics::ToolHelp::MODULEENTRY32W;
+use windows::Win32::System::Diagnostics::ToolHelp::PROCESSENTRY32W;
+use windows::Win32::System::Diagnostics::ToolHelp::{
+    TH32CS_SNAPMODULE, TH32CS_SNAPMODULE32, TH32CS_SNAPPROCESS,
 };
 
+use windows::Win32::System::Threading::OpenProcess;
+use windows::Win32::System::Threading::PROCESS_ALL_ACCESS;
+
+use windows::Win32::System::ProcessStatus::K32GetModuleFileNameExW;
+
+use windows::Win32::System::Memory::{
+    VirtualAllocEx, VirtualQueryEx, MEMORY_BASIC_INFORMATION, MEM_COMMIT, MEM_IMAGE, MEM_RESERVE,
+    PAGE_EXECUTE_READWRITE,
+};
+
+#[derive(Debug)]
+pub struct Process_ {
+    pub id: u32,
+    pub name: String,
+}
 
 #[derive(Debug)]
 pub struct Process {
-    pub process_id: u32,
-    pub handle: HANDLE,
+    pub id: u32,
+    pub name: String,
+    pub handle: isize,
+    pub path: String,
 }
 
-unsafe impl Send for Process {}
-unsafe impl Sync for Process {}
+#[derive(Debug)]
+pub struct Module {
+    pub base_address: u64,
+    pub size: usize,
+    pub name: String,
+    pub path: String,
+}
 
-impl Process {
-    pub fn new(proc: &str) -> Result<Process, Box<dyn std::error::Error>> {
-        let hProcessId = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
+pub fn open_process(process_name: &str) -> Result<Process, Box<dyn std::error::Error>> {
+    unsafe {
+        let hProcessId = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)?;
         let mut entry: PROCESSENTRY32W = unsafe { std::mem::zeroed() };
-        entry.dwSize = size_of::<PROCESSENTRY32W>() as u32;
+        entry.dwSize = mem::size_of::<PROCESSENTRY32W>() as u32;
         let mut found = false;
-        while unsafe { Process32NextW(hProcessId, &mut entry) } != 0 {
-            if entry
-                .szExeFile
-                .iter()
-                .take_while(|&x| *x != 0)
-                .map(|&x| x as u8 as char)
-                .collect::<String>()
-                == proc
-            {
+        while Process32NextW(hProcessId, &mut entry).as_bool() {
+            if wchar_to_string(&entry.szExeFile) == process_name.to_string() {
                 found = true;
                 break;
             }
         }
-        if !found {
-            return Err("Process not found".into());
+        if (!found) {
+            return Err("Failed to open process".into());
         }
-        let process_id = entry.th32ProcessID;
-        let handle = unsafe {
-            let handle = OpenProcess(PROCESS_ALL_ACCESS, 0, process_id);
-            if handle == std::ptr::null_mut() {
-                return Err("Failed to open process".into());
-            }
-            handle
-        };
-        Ok(Process { handle, process_id })
-    }
-
-    pub fn close_handle(&self) {
-        unsafe {
-            CloseHandle(self.handle);
+        CloseHandle(hProcessId);
+        let hProcess = OpenProcess(PROCESS_ALL_ACCESS, BOOL(0), entry.th32ProcessID)?;
+        let mut path: [u16; 260] = [0; 260];
+        let mut ret = K32GetModuleFileNameExW(hProcess, HINSTANCE(0), &mut path);
+        if ret == 0 {
+            return Err("Failed to get process path".into());
         }
-    }   
-
-    pub fn read_memory(
-        &self,
-        address: u64,
-        size: usize,
-    ) -> Vec<u8> {
-        let mut buffer = vec![0; size];
-        unsafe {
-            ReadProcessMemory(
-                self.handle,
-                address as *mut _,
-                buffer.as_mut_ptr() as *mut _,
-                size,
-                ptr::null_mut(),
-            ); 
-        };
-        buffer
+        let path = wchar_to_string(&path);
+        return Ok(Process {
+            id: entry.th32ProcessID,
+            name: wchar_to_string(&entry.szExeFile),
+            handle: hProcess.0,
+            path,
+        });
     }
-
-    pub fn write_memory(
-        &self,
-        address: u64,
-        data: &[u8],
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let size = data.len();
-        let written = unsafe {
-            WriteProcessMemory(
-                self.handle,
-                address as *mut _,
-                data.as_ptr() as *mut _,
-                size,
-                ptr::null_mut(),
-            )
-        };
-        if written == 0 {
-            return Err("Failed to write memory".into());
-        };
-        Ok(())
-    }
-
-    pub fn sig_scan(&self, pattern: &str, start_address: u64) -> Option<u64> {
-        let mut info: MEMORY_BASIC_INFORMATION = unsafe { std::mem::zeroed() };
-        let size = size_of::<MEMORY_BASIC_INFORMATION>() as usize;
-        let mut address = start_address;
-        loop {
-            unsafe {
-                if VirtualQueryEx(
-                    self.handle,
-                    address as *mut _,
-                    &mut info,
-                    size,
-                ) != size
-                {
-                    return None;
-                }
-                address = info.BaseAddress as u64 + info.RegionSize as u64;
-                if info.State != MEM_COMMIT || info.Type == MEM_IMAGE {
-                    continue;
-                }
-                let _buffer = self
-                    .read_memory(info.BaseAddress as u64, info.RegionSize);
-                let sig = Signature::from(pattern.split(" ").map(|x| {
-                    match x {
-                        "?" => None,
-                        _ => Some(u8::from_str_radix(x, 16).unwrap()),
-                    }
-                }).collect::<Vec<Option<u8>>>());
-                match sig.scan(&_buffer) {
-                    Some(x) => {
-                        return Some(info.BaseAddress as u64 + x as u64);
-                    }
-                    None => {},
-                };
-            }
-        }
-    }
-
 }
 
-impl Finalize for Process {}
+pub fn read_memory_buffer(handle: HANDLE, address: u64, size: usize) -> Vec<u8> {
+    let mut buffer: Vec<u8> = vec![0; size];
+    unsafe {
+        ReadProcessMemory(
+            handle,
+            address as *mut _,
+            &mut buffer as *mut _ as *mut _,
+            size,
+            ptr::null_mut(),
+        );
+    }
+    buffer
+}
+
+pub fn read_memory<T>(handle: HANDLE, address: u64) -> T {
+    let mut val: T = unsafe { std::mem::zeroed() };
+    let size = std::mem::size_of::<T>();
+    unsafe {
+        ReadProcessMemory(
+            handle,
+            address as *mut _,
+            &mut val as *mut _ as *mut _,
+            size,
+            ptr::null_mut(),
+        );
+    }
+    val
+}
+
+pub fn write_memory_buffer(handle: HANDLE, aaddress: u64, buffer: &Vec<u8>) {
+    let size = buffer.len();
+    unsafe {
+        WriteProcessMemory(
+            handle,
+            aaddress as *mut _,
+            buffer as *const _ as *mut _,
+            size,
+            ptr::null_mut(),
+        );
+    }
+}
+
+pub fn write_memory<T>(handle: HANDLE, aaddress: u64, val: T) {
+    let size = std::mem::size_of::<T>();
+    unsafe {
+        WriteProcessMemory(
+            handle,
+            aaddress as *mut _,
+            &val as *const _ as *mut _,
+            size,
+            ptr::null_mut(),
+        );
+    }
+}
+
+pub fn alloc_memory(handle: HANDLE, size: usize) -> u64 {
+    let address = unsafe {
+        VirtualAllocEx(
+            handle,
+            ptr::null_mut(),
+            size,
+            MEM_COMMIT | MEM_RESERVE,
+            PAGE_EXECUTE_READWRITE,
+        )
+    };
+
+    let a = format!("{:?}", address);
+    u64::from_str_radix(&a[2..], 16).unwrap()
+}
+
+pub fn close_handle(handle: HANDLE) -> bool {
+    unsafe { CloseHandle(handle).as_bool() }
+}
+
+pub fn get_modules(process_id: u32) -> Result<Vec<Module>, Box<dyn std::error::Error>> {
+    unsafe {
+        let hProcessId =
+            CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, process_id)?;
+        let mut entry: MODULEENTRY32W = unsafe { std::mem::zeroed() };
+        entry.dwSize = mem::size_of::<MODULEENTRY32W>() as u32;
+        let mut modules = Vec::new();
+        while Module32NextW(hProcessId, &mut entry).as_bool() {
+            let module = Module {
+                base_address: entry.modBaseAddr as u64,
+                size: entry.modBaseSize as usize,
+                name: wchar_to_string(&entry.szModule),
+                path: wchar_to_string(&entry.szExePath),
+            };
+            modules.push(module);
+        }
+        CloseHandle(hProcessId);
+        return Ok(modules);
+    }
+}
+
+pub fn get_processes() -> Result<Vec<Process_>, Box<dyn std::error::Error>> {
+    unsafe {
+        let hProcessId = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)?;
+        let mut entry: PROCESSENTRY32W = unsafe { std::mem::zeroed() };
+        entry.dwSize = mem::size_of::<PROCESSENTRY32W>() as u32;
+        let mut processes = Vec::new();
+        while Process32NextW(hProcessId, &mut entry).as_bool() {
+            let process = Process_ {
+                id: entry.th32ProcessID,
+                name: wchar_to_string(&entry.szExeFile),
+            };
+            processes.push(process);
+        }
+        CloseHandle(hProcessId);
+        return Ok(processes);
+    }
+}
+
+pub fn sig_scan(handle: HANDLE, pattern: &str, start_address: u64) -> Option<u64> {
+    let mut info: MEMORY_BASIC_INFORMATION = unsafe { std::mem::zeroed() };
+    let size = mem::size_of::<MEMORY_BASIC_INFORMATION>() as usize;
+    let mut address = start_address;
+    loop {
+        unsafe {
+            if VirtualQueryEx(handle, address as *mut _, &mut info, size) != size {
+                return None;
+            }
+            address = info.BaseAddress as u64 + info.RegionSize as u64;
+            if info.State != MEM_COMMIT || info.Type == MEM_IMAGE {
+                continue;
+            }
+            let mut buffer: Vec<u8> = vec![0; info.RegionSize as usize];
+            ReadProcessMemory(
+                handle,
+                info.BaseAddress as *mut _,
+                buffer.as_mut_ptr() as *mut _,
+                info.RegionSize as usize,
+                ptr::null_mut(),
+            );
+
+            let sig = Signature::from(
+                pattern
+                    .split(" ")
+                    .map(|x| match x {
+                        "?" => None,
+                        _ => Some(u8::from_str_radix(x, 16).unwrap()),
+                    })
+                    .collect::<Vec<Option<u8>>>(),
+            );
+            match sig.scan(&buffer) {
+                Some(x) => {
+                    return Some(info.BaseAddress as u64 + x as u64);
+                }
+                None => {}
+            };
+        }
+    }
+}
+
+fn wchar_to_string(wchar: &[u16]) -> String {
+    wchar
+        .iter()
+        .take_while(|&x| *x != 0)
+        .map(|&x| x as u8 as char)
+        .collect::<String>()
+}
 
 // cargo test -- --nocapture
 #[test]
-fn check_sig_scan() {
-    let process = Process::new("osu!.exe").unwrap();
-    println!("{:?}", process);
-    // process.read_memory(0x106b000, 12288);
-    let find = process.sig_scan("64 00 00 00 00 00 00 00 18 5A 55 01 01 00 00 00 00 00 00 00", 0);
-    match find {
-        Some(x) => println!("{:x}", x),
-        None => println!("Not found"),
-    };
-    assert_eq!(2 + 2, 4);
-}
-
-#[test]
-fn keystone() {
-    // let engine = Keystone::new(Arch::X86, Mode::Mode32)
-    //     .expect("Could not initialize Keystone engine");
-
-    // engine.option(OptionType::Syntax, OptionValue::SyntaxNASM);
-    //     // .expect("Could not set option to nasm syntax");
-
-    // // let result = engine.asm("mov ah, 0x80".to_string(), 0)
-    // //     .expect("Could not assemble");
-
-    // print!("{}", result);
-    assert_eq!(2 + 2, 4);
+fn allocate_memory() {
+    let process = open_process("Discord.exe").unwrap();
+    let handle = HANDLE(process.handle);
+    let address = alloc_memory(handle, 10);
+    write_memory(handle, address, 0x02);
+    let val = read_memory::<u8>(handle, address);
+    assert_eq!(val, 2);
 }
